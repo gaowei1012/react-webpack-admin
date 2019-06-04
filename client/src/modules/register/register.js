8fbefa99be5297c70e3a3bd922dc5f0301d531e2
gmw-zjw <mingwei2222@126.com>
2019-06-04 19:34:57 +0800

    完成注册页面UI初始化

--- client/src/modules/register/register.js
+++ client/src/modules/register/register.js
@@ -5,7 +5,7 @@ import RegisterForm from './components/RegisterForm'
 export default class Register extends PureComponent {
   render() {
     return (
-      <div><RegisterForm /></div>
+      <div><RegisterForm  /></div>
     )
   }
 }
