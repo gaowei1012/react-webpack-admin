6ca8dcb62b2cb1ab74cf99daf1bae86d4da43f75
gmw-zjw <mingwei2222@126.com>
2019-06-04 19:35:40 +0800

    修改部分代码

--- client/src/modules/register/register.js
+++ client/src/modules/register/register.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
 import RegisterForm from './components/RegisterForm'
 
 export default class Register extends PureComponent {
