0bfb140e837a4bf7864e068940cc6249dc1762a9
gmw-zjw <mingwei2222@126.com>
2019-06-10 18:03:37 +0800

    添加登录权限校验

---
+++ src/app/routes/AuthorizedRoute.js
@@ -0,0 +1,18 @@
+// 权限控制
+import React from 'react'
+import {
+  Redirect,
+  Route
+} from 'react-router-dom'
+
+class AuthorizedRoute extends React.Component {
+  // authorized route
+  render() {
+    return (
+      // TODDO
+      <span>authorized</span>
+    )
+  }
+}
+
+export default AuthorizedRoute
