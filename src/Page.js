19741b605450fde610d8459cd762872be6dc8741
gmw-zjw <mingwei2222@126.com>
2019-06-11 15:42:10 +0800

    添加登录页面逻辑

--- src/Page.js
+++ src/Page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import App from './app/App';
+import Login from './app/components/Login/Login';
+import NotFound from './app/components/NotFound/NotFound';
 
 class Page extends React.Component {
   render() {
@@ -9,6 +11,9 @@ class Page extends React.Component {
         <Switch>
           <Route path='/' render={() => <Redirect to='/app/dashboard/index' push/>} />
           <Route path='/app' component={App} />
+          <Route path='/404' component={NotFound} />
+          <Route path='/login' component={Login} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     );
