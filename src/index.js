0eee39e393ffe5e75fb5f2e43bd6301fb8dfdff2
gmw-zjw <mingwei2222@126.com>
2019-06-11 15:39:30 +0800

    添加redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './Page';
+import store from './app/redux/store';
+import { Provider } from 'react-redux';
 // antd
 import './styles/antd.css';
 
-ReactDOM.render(<Page />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Page />
+  </Provider>
+  , document.querySelector('#root'));
