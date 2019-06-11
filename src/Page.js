55fc4d2ee5db776cb1ced125cec2e6a417ad4fe6
gmw-zjw <mingwei2222@126.com>
2019-06-11 16:13:13 +0800

    改用BrowserRouter

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import App from './app/App';
 import Login from './app/components/Login/Login';
 import NotFound from './app/components/NotFound/NotFound';
