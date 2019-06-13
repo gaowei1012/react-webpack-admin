1f8a610478c4bb366a3517a529c88fe4f4cce276
gmw-zjw <mingwei2222@126.com>
2019-06-13 17:02:09 +0800

    富文本ui出成

--- src/app/components/ui/Wysiwygs.js
+++ src/app/components/ui/Wysiwygs.js
@@ -1,9 +1,36 @@
+/**
+ * 富文本这里采用braft-editor这款，很棒
+ * GitHub: https://github.com/margox/braft-editor
+ */
+
 import React from 'react';
+import { Row, Col } from 'antd';
+import BraftEditor from 'braft-editor';
+import BreadcrumbCustom from './../BreadcrumbCustom';
+import 'braft-editor/dist/index.css'; // css样式文件
+
 
 class Wysiwygs extends React.Component {
+
+  state = {
+    efitorState: BraftEditor.createEditorState(null)
+  }
+
+  handelChange = (efitorState) => {
+    this.setState({
+      efitorState
+    })
+  }
+
   render() {
+    const { efitorState } = this.state;
     return(
-      <div>this is wysiwyg</div>
+      <div>
+        <BreadcrumbCustom first='UI' second='富文本'/>
+        <Row style={{marginTop: '20px'}}>
+          <BraftEditor value={efitorState} onChange={this.handelChange}></BraftEditor>
+        </Row>
+      </div>
     )
   }
 }
