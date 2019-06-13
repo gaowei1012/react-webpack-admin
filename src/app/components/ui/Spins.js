223c2d7cb3ac111a6f7b38c701972df43d3bccda
gmw-zjw <mingwei2222@126.com>
2019-06-13 16:04:53 +0800

    spin页面bug修复

--- src/app/components/ui/Spins.js
+++ src/app/components/ui/Spins.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Spin } from 'antd';
+import { Spin, Col, Row, Card, Alert } from 'antd';
+import BreadcrumbCustom from './../BreadcrumbCustom';
 
 class Spins extends React.Component {
   render() {
     return(
       <div>
-        this is spins page
+        <BreadcrumbCustom first='UI' second='加载中' />
+        <Row style={{marginTop: '20px'}}>
+          <Col span={10}>
+            <Card title='基本用法'>
+              <Spin />
+            </Card>
+          </Col>
+          <Col span={10} style={{marginLeft: '20px'}}>
+            <Card title='自定义文案'>
+            <Spin tip="Loading...">
+              <Alert
+                message="Alert message title"
+                description="Further details about the context of this alert."
+                type="info"
+              />
+              </Spin>
+            </Card>
+          </Col>
+        </Row>
+        <Row style={{marginTop: '30px'}}>
+          <Col span={10}>
+            <Card title='自定义指示符'>
+              <Spin indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />} />
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
