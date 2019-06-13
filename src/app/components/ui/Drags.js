4a1894c3a3d9b1578e4364c2ed9bfa993a2f10d0
gmw-zjw <mingwei2222@126.com>
2019-06-13 17:26:25 +0800

    实现拖拽基本功能

--- src/app/components/ui/Drags.js
+++ src/app/components/ui/Drags.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 
 class Drags extends React.Component {
+
+  hanleStart = () => {
+
+  }
+
+  handleDrag = () => {
+
+  }
+
+  handleStop = () => {
+
+  }
+
   render() {
     return (
-      <div>this is Drags</div>
+      <div>
+        <Draggable
+          axis='x'
+          handle='.handle'
+          defaultPosition={{x: 0, y:0}}
+          position={null}
+          grid={[25, 25]}
+          scale={1}
+          onStart={this.hanleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}>
+            <div>
+              <div className="handle" style={{width: '200px', height: '200px', border: '1px solid #ccc', background: '#ccc'}}>Drag from here</div>
+              {/* <div style={{width: '200px', height: '200px', border: '1px solid #ccc', background: 'red'}}>This readme is really dragging on...</div> */}
+            </div>
+          </Draggable>
+      </div>
     )
   }
 }
