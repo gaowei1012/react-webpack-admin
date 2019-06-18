15d02348bb91d48a11798ed5641b45e1d46ce737
gmw-zjw <mingwei2222@126.com>
2019-06-18 12:49:17 +0800

    通知提醒bug修复

--- src/app/components/ui/Notifications.js
+++ src/app/components/ui/Notifications.js
@@ -66,13 +66,13 @@ class Notifications extends React.Component {
       <div>
         <BreadcrumbCustom first='UI' second='通知提醒' />
         <Row style={{marginTop: '30px'}}>
-          <Col span={10}>
+          <Col span={12}>
             <Card title='基础用法'>
               <Button type='default' onClick={this.openNotification}>Open Notification</Button>
             </Card>
           </Col>
-          <Col span={10} style={{marginLeft: '30px'}}>
-            <Card title='带有图标'>
+          <Col span={12}>
+            <Card title='带有图标' style={{marginLeft: '10px'}}>
               <Button type='default' onClick={this.Info}>Info</Button>
               <Button type='default' onClick={this.Success}>Success</Button>
               <Button type='default' onClick={this.Warning}>Warning</Button>
@@ -81,7 +81,7 @@ class Notifications extends React.Component {
           </Col>
         </Row>
         <Row style={{marginTop: '30px'}}>
-          <Col span={10}>
+          <Col span={12}>
             <Card title='自定义把图标'>
               <Button type='default' onClick={this.diyNotification}>
                 自定义图标
