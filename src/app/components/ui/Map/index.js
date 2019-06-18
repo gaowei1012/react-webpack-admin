f397d31e00d7925f47e9a9ba0149c2084f98f698
gmw-zjw <mingwei2222@126.com>
2019-06-18 12:58:18 +0800

    map

--- src/app/components/ui/Map/index.js
+++ src/app/components/ui/Map/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Map } from 'react-amap';
-import { Row, Col, Card } from 'antd';
+import { Row, Card } from 'antd';
 import BreadcrumbCustom from './../../BreadcrumbCustom';
 
 /**
